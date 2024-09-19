3c0cfb5d2e9be6aa01cc14dc5407efd1f930dbfd
nobelnithin <nithinnithin2812@gmail.com>
2024-09-19 09:16:44 +0530

    first commit

---
+++ main.c
@@ -0,0 +1,53 @@
+ /*
+ * MAIN Generated Driver File
+ * 
+ * @file main.c
+ * 
+ * @defgroup main MAIN
+ * 
+ * @brief This is the generated driver implementation file for the MAIN driver.
+ *
+ * @version MAIN Driver Version 1.0.0
+*/
+
+/*
+© [2024] Microchip Technology Inc. and its subsidiaries.
+
+    Subject to your compliance with these terms, you may use Microchip 
+    software and any derivatives exclusively with Microchip products. 
+    You are responsible for complying with 3rd party license terms  
+    applicable to your use of 3rd party software (including open source  
+    software) that may accompany Microchip software. SOFTWARE IS ?AS IS.? 
+    NO WARRANTIES, WHETHER EXPRESS, IMPLIED OR STATUTORY, APPLY TO THIS 
+    SOFTWARE, INCLUDING ANY IMPLIED WARRANTIES OF NON-INFRINGEMENT,  
+    MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE. IN NO EVENT 
+    WILL MICROCHIP BE LIABLE FOR ANY INDIRECT, SPECIAL, PUNITIVE, 
+    INCIDENTAL OR CONSEQUENTIAL LOSS, DAMAGE, COST OR EXPENSE OF ANY 
+    KIND WHATSOEVER RELATED TO THE SOFTWARE, HOWEVER CAUSED, EVEN IF 
+    MICROCHIP HAS BEEN ADVISED OF THE POSSIBILITY OR THE DAMAGES ARE 
+    FORESEEABLE. TO THE FULLEST EXTENT ALLOWED BY LAW, MICROCHIP?S 
+    TOTAL LIABILITY ON ALL CLAIMS RELATED TO THE SOFTWARE WILL NOT 
+    EXCEED AMOUNT OF FEES, IF ANY, YOU PAID DIRECTLY TO MICROCHIP FOR 
+    THIS SOFTWARE.
+*/
+#include "mcc_generated_files/system/system.h"
+#include <util/delay.h>
+/*
+    Main application
+*/
+#define LED_PIN 6
+#define BUZZER_PIN 1
+
+int main(void)
+{
+    SYSTEM_Initialize();
+    PORTA_set_pin_dir(LED_PIN, PORT_DIR_OUT);
+    PORTA_set_pin_dir(BUZZER_PIN, PORT_DIR_OUT);
+    
+    while(1)
+    {
+        PORTA_toggle_pin_level(BUZZER_PIN);
+        PORTA_toggle_pin_level(LED_PIN);
+        _delay_ms(500);
+    }    
+}
---
+++ mcc_generated_files/system/src/pins.c
@@ -0,0 +1,94 @@
+/**
+ * Generated Driver File
+ * 
+ * @file pins.c
+ * 
+ * @ingroup  pinsdriver
+ * 
+ * @brief This is generated driver implementation for pins. 
+ *        This file provides implementations for pin APIs for all pins selected in the GUI.
+ *
+ * @version Driver Version 1.1.0
+*/
+
+/*
+© [2024] Microchip Technology Inc. and its subsidiaries.
+
+    Subject to your compliance with these terms, you may use Microchip 
+    software and any derivatives exclusively with Microchip products. 
+    You are responsible for complying with 3rd party license terms  
+    applicable to your use of 3rd party software (including open source  
+    software) that may accompany Microchip software. SOFTWARE IS ?AS IS.? 
+    NO WARRANTIES, WHETHER EXPRESS, IMPLIED OR STATUTORY, APPLY TO THIS 
+    SOFTWARE, INCLUDING ANY IMPLIED WARRANTIES OF NON-INFRINGEMENT,  
+    MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE. IN NO EVENT 
+    WILL MICROCHIP BE LIABLE FOR ANY INDIRECT, SPECIAL, PUNITIVE, 
+    INCIDENTAL OR CONSEQUENTIAL LOSS, DAMAGE, COST OR EXPENSE OF ANY 
+    KIND WHATSOEVER RELATED TO THE SOFTWARE, HOWEVER CAUSED, EVEN IF 
+    MICROCHIP HAS BEEN ADVISED OF THE POSSIBILITY OR THE DAMAGES ARE 
+    FORESEEABLE. TO THE FULLEST EXTENT ALLOWED BY LAW, MICROCHIP?S 
+    TOTAL LIABILITY ON ALL CLAIMS RELATED TO THE SOFTWARE WILL NOT 
+    EXCEED AMOUNT OF FEES, IF ANY, YOU PAID DIRECTLY TO MICROCHIP FOR 
+    THIS SOFTWARE.
+*/
+
+#include "../pins.h"
+
+static void (*LED_InterruptHandler)(void);
+
+void PIN_MANAGER_Initialize()
+{
+
+  /* OUT Registers Initialization */
+    PORTA.OUT = 0x2;
+
+  /* DIR Registers Initialization */
+    PORTA.DIR = 0x2;
+
+  /* PINxCTRL registers Initialization */
+    PORTA.PIN0CTRL = 0x0;
+    PORTA.PIN1CTRL = 0x0;
+    PORTA.PIN2CTRL = 0x0;
+    PORTA.PIN3CTRL = 0x0;
+    PORTA.PIN4CTRL = 0x0;
+    PORTA.PIN5CTRL = 0x0;
+    PORTA.PIN6CTRL = 0x0;
+    PORTA.PIN7CTRL = 0x0;
+
+  /* PORTMUX Initialization */
+    PORTMUX.CTRLA = 0x0;
+    PORTMUX.CTRLB = 0x0;
+    PORTMUX.CTRLC = 0x0;
+    PORTMUX.CTRLD = 0x0;
+
+  // register default ISC callback functions at runtime; use these methods to register a custom function
+    LED_SetInterruptHandler(LED_DefaultInterruptHandler);
+}
+
+/**
+  Allows selecting an interrupt handler for LED at application runtime
+*/
+void LED_SetInterruptHandler(void (* interruptHandler)(void)) 
+{
+    LED_InterruptHandler = interruptHandler;
+}
+
+void LED_DefaultInterruptHandler(void)
+{
+    // add your LED interrupt custom code
+    // or set custom function using LED_SetInterruptHandler()
+}
+ISR(PORTA_PORT_vect)
+{ 
+    // Call the interrupt handler for the callback registered at runtime
+    if(VPORTA.INTFLAGS & PORT_INT1_bm)
+    {
+       LED_InterruptHandler(); 
+    }
+    /* Clear interrupt flags */
+    VPORTA.INTFLAGS = 0xff;
+}
+
+/**
+ End of File
+*/
